232d96ed7d47843b471505bf1ecd373099907618
Scott Williams <scottcolemanwilliams@gmail.com>
2019-03-19 10:56:03 -0500

    added snapshot testing

--- src/Card.test.js
+++ src/Card.test.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Card from './Card.js';
+import renderer from 'react-test-renderer';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Card title="x" content="z"/>, div);
-  ReactDOM.unmountComponentAtNode(div);
+
+describe('<Card />', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Card title="x" content="z"/>, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+  
+  it('matches snapshot', () => {
+    const tree =renderer.create(<Card title='x' content='z'/>).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 });
--- src/List.test.js
+++ src/List.test.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import List from './Card.js';
+import List from './List.js';
+import renderer from 'react-test-renderer';
+import STORE from './STORE'
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<List header="x" cards={[1, 2]}/>, div);
+  ReactDOM.render(<List header='Fourth list' cards={['l', 'm']}/>, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+it('matches snapshot', () => {
+  const tree =renderer.create(<List header='Fourth list' cards={['l', 'm']}/>).toJSON();
+  expect(tree).toMatchSnapshot();
 });
